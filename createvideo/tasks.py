ef0526709793480501e15715b73b1f2053f54820
Asem Yousry <asemgeeklabs@gmail.com>
2024-12-26 13:42:42 +0200

    append audio to intro clip

--- createvideo/tasks.py
+++ createvideo/tasks.py
@@ -29,12 +29,13 @@ def bodytest(slides_list,body_list,webhook):
         ## download image ##
         response = requests.get(image_url)
         image_data = BytesIO(response.content)
-        # image_data = download_image(image_url)
         ### create slide content ###
         componant = methods_list[index](image_path=image_data,duration=duration,text=text,start=start)
         list_componant.extend(componant)
         list_audios.append((audio_url,start))
+    intro_audio = AudioFileClip("downloads/intro_audio.mp3").with_start(slides_list[-1]["start_time"]+slides_list[-1]["duration"])
     list_audios_instance = add_audios(list_audios)
+    list_audios_instance.append(intro_audio)
     ### start body process ###
     path = body(body_list=body_list,clips=list_componant,audio_clips=list_audios_instance,video_name=video_name)
     url =  settings.MEDIA_URL + path
