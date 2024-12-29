1424499a1db4d7c278a0a3de2354ed693681d956
Asem Yousry <asemgeeklabs@gmail.com>
2024-12-29 16:30:00 +0200

    make api close instance

--- createvideo/urls.py
+++ createvideo/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import CreateVideo , Testslide4
 
 urlpatterns = [
-    path('create_video/', CreateVideo.as_view()),
+    path('', CreateVideo.as_view()),
     path('testslide4/', Testslide4.as_view()),
 ]
--- street_politics/urls.py
+++ street_politics/urls.py
@@ -3,5 +3,5 @@ from django.urls import path , include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('street_politics/', include('createvideo.urls')),
+    path('create_video/', include('createvideo.urls')),
 ]
