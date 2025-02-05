8d2492b7cf0a78094dc1e2ea5b63c6d61f6dac3a
Asem Yousry <asemgeeklabs@gmail.com>
2025-02-05 10:23:05 +0200

    duration of text and text bar

--- logic/intro/intro_methods.py
+++ logic/intro/intro_methods.py
@@ -196,12 +196,12 @@ def Slide4(image_path,text,start,duration):
         interline= 30,
         horizontal_align="left",
         # margin=(50,10) ,
-    ).with_position(lambda t : method_motion(t=t,x=340,y=3180,total_distance=(3180-400),slow_ratio=slow_ratio_text)).with_start(start-4).with_duration(duration+8)
+    ).with_position(lambda t : method_motion(t=t,x=340,y=3180,total_distance=(3180-400),slow_ratio=slow_ratio_text)).with_start(start-4).with_duration(duration+6)
 
     ## internal text width of text4 ##
     internal_text_width = get_internal_text_width(text=text4)
     #### text color base ####
-    text_base_color = ColorClip((internal_text_width,20),color=(170,36,30)).with_position(lambda t : method_motion(t=t,x=340,y=3180,total_distance=(3180-(350+text4.h+50)),slow_ratio=slow_ratio_text+0.1)).with_start(start-4).with_duration(duration+8)
+    text_base_color = ColorClip((internal_text_width,20),color=(170,36,30)).with_position(lambda t : method_motion(t=t,x=340,y=3180,total_distance=(3180-(350+text4.h+50)),slow_ratio=slow_ratio_text+0.1)).with_start(start-4).with_duration(duration+6)
     ### intro clip of street politics ###
     intro = VideoFileClip("downloads/Street_Politics_intro.mov", has_mask=True,target_resolution=(1920,1080)).with_start(start+(duration-2))
     return [gray_background, left_layer_gray,middle_layer_gray,right_layer_offwhite,right_layer_lightgray,left_layer_black,
